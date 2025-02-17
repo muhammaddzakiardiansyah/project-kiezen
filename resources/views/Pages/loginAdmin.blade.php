2c910f29c3210ed4f90aeb4692abc45813ef4d94
muhammaddzakiardiansyah <muhammaddzakiardiansyah811@gmail.com>
2025-02-17 22:22:24 +0700

    add: page login user & login admin

---
+++ resources/views/Pages/loginAdmin.blade.php
@@ -0,0 +1,26 @@
+@extends('Layouts.main')
+
+@section('main')
+    <section class="w-screen h-screen">
+        <div class="flex">
+            <div class="flex-1 w-full h-screen">
+                <div class="w-[80%] h-screen mx-auto flex flex-col justify-center items-center">
+                    <h3 class="text-3xl font-bold text-[#3A3434] my-5">Login Admin</h3>
+                    <form action="" class="w-[65%]" method="post">
+                        <div class="mb-3">
+                            <label for="email" class="block font-semibold text-[#3A3434] mb-3">Email</label>
+                            <input type="email" class="px-5 py-3 placeholder:font-medium ring-2 ring-[#E76E6E] outline-2 outline-[#E76E6E] rounded-full bg-[#F8F8F8] w-full" name="email" placeholder="Enter Email" id="email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="block font-semibold text-[#3A3434] mb-3">Password</label>
+                            <input type="password" class="px-5 py-3 placeholder:font-medium ring-2 ring-[#E76E6E] outline-2 outline-[#E76E6E] rounded-full bg-[#F8F8F8] w-full" name="password" placeholder="Enter Password" id="password">
+                        </div>
+                        <button type="submit" class="px-16 border cursor-pointer py-2 bg-[#F8F8F8] font-bold text-[#3A3434] rounded-full mt-14 block mx-auto">Login</button>
+                    </form>
+                    <p class="text-sm font-semibold mt-10">Is User ? <a href="/login-user" class="text-blue-600">Login Here</a></p>
+                </div>
+            </div>
+            <div class="flex-1 bg-[#E76E6E] w-full h-screen"></div>
+        </div>
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Pages.homePage');
 });
+
+Route::get('/login-user', function() {
+    return view('Pages.loginUserPage');
+});
+
+Route::get('/login-admin', function() {
+    return view('Pages.loginAdmin');
+});
